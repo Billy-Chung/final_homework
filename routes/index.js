c013db223d8e08149e8f1da34d687744f2181b73
鄭又誠 <zxcy9980@gmail.com>
2020-05-25 16:06:13 +0800

    refactor: modify express routes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+// 準備引入路由模組
+
+const home = require('./modules/hmoe')
+router.use('/', home)
+// 匯出路由器
+module.exports = router
---
+++ routes/modules/hmoe.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router()
+const Shortener = require('../../modules/shortener')
+const  getShortUrl = require('../../getShortUrl') 
+
+
+
+// 設定首頁路由
+router.get('/', (req, res) => {
+    res.render('index')
+  })
+
+  //設定創建T路由
+  router.post('/', (req, res,  next) => {
+    let inputUrl = req.body.inputUrl
+  
+    //產生短網址
+    let fakerUrl = getShortUrl()  
+  
+    return Shortener.find()
+      .then(check => {
+        // 需要防止有重覆的網址組合出現
+        let checkBox = []
+        check.forEach(item => {
+          checkBox.push(item.fakerUrl)
+        })
+        while (checkBox.includes(fakerUrl)) {
+          fakerUrl = generateShortURL()
+        }
+        return fakerUrl
+      })
+      .then(() => Shortener.create({ fakerUrl :fakerUrl, url : inputUrl }))
+      .then(() => res.render('show', { inputUrl, fakerUrl }))
+      .catch(error => console.log(error))
+    })
+    
+    router.get('/:id', (req, res) => {
+        const id = req.params.id
+        Shortener.find({ fakerUrl: id })
+          .lean()
+          .then(xxx => {
+            newUrl = xxx[0].url      
+            res.redirect(`${newUrl}`)
+          })
+          .catch(error => console.log(error))
+      
+      })
+
+      module.exports = router
